5714c253002ea248bd99544dc2f5e18d380ef982
TERI <69073505+TERIHAX@users.noreply.github.com>
2022-10-02 19:20:32 +0100

    Add files via upload

---
+++ KillDesigner/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+
+foreach (Process process in Process.GetProcesses())
+{
+    switch (process.ProcessName.ToLower())
+    {
+        case "xamldesigner":
+            process.Kill();
+            break;
+
+        case "xamldesigner launcher":
+            process.Kill();
+            break;
+    }
+}
+
+foreach (Process process in Process.GetProcessesByName("XamlDesigner"))
+{
+    process.Kill();
+}
+
+Console.WriteLine("XAMLDesigner tasks killed.");
+Environment.Exit(0);
